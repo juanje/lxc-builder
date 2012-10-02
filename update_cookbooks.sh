d22111bbfc5f517af0e9cd1d7f6e620aed0dca1a
Ayose <ayosec@gmail.com>
2012-10-02 16:52:02 +0100

    Use master instead of the commit hashes
    I was having some issues checking out that references.
    
    The `-b X --depth 1` combination was failing in Git 1.7.10.4

--- update_cookbooks.sh
+++ update_cookbooks.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+set -e  # Exit after any error
+
 rm -fr cookbooks
 mkdir cookbooks
 pushd cookbooks
-git clone -b "1.4.4"   --depth 1 git://github.com/opscode-cookbooks/apt.git          apt
-git clone -b "1.5.2"   --depth 1 git://github.com/opscode-cookbooks/java.git         java
-git clone -b "26fd53d" --depth 1 git://github.com/opscode-cookbooks/openssl.git      openssl
-git clone -b "dbf5e44" --depth 1 git://github.com/opscode-cookbooks/postgresql.git   postgresql
-git clone -b "1.2.0"   --depth 1 git://github.com/opscode-cookbooks/sudo.git         sudo
-git clone -b "0.11.0"  --depth 1 git://github.com/edelight/chef-mongodb.git          mongodb
-git clone -b "v0.9.0"  --depth 1 git://github.com/fnichol/chef-rvm.git               rvm
-git clone -b "ba2e875" --depth 1 git://github.com/juanje/cookbook-conf.git           conf
-git clone -b "HEAD"    --depth 1 git@github.com:juanje/cookbook-aentos-bootstrap.git aentos-bootstrap
+git clone -b "1.4.4"   git://github.com/opscode-cookbooks/apt.git          apt
+git clone -b "1.5.2"   git://github.com/opscode-cookbooks/java.git         java
+git clone -b "master"  git://github.com/opscode-cookbooks/openssl.git      openssl
+git clone -b "master"  git://github.com/opscode-cookbooks/postgresql.git   postgresql
+git clone -b "1.2.0"   git://github.com/opscode-cookbooks/sudo.git         sudo
+git clone -b "0.11.0"  git://github.com/edelight/chef-mongodb.git          mongodb
+git clone -b "v0.9.0"  git://github.com/fnichol/chef-rvm.git               rvm
+git clone -b "master"  git://github.com/juanje/cookbook-conf.git           conf
+git clone -b "master"  https://github.com/juanje/cookbook-aentos-bootstrap.git aentos-bootstrap
 popd
-tar --exclude-vcs -zcvf chef-solo.tar.gz ./cookbooks
 
+tar --exclude-vcs -zcvf chef-solo.tar.gz cookbooks
